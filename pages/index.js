94599321a70705adae3c0148099b8ee836d0bab4
mkngroup <asparpazarlamamehmet@gmail.com>
2022-05-10 20:11:09 +0300

    after deploy fb verification icon add

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,8 @@ export default function Home() {
       <Head>
         <title>Quins Solutions</title>
         <meta name="description" content="DERMOKOZMETIĞIN ÖNCÜ MARKASI QUINS DERM SOLUTIONS® CILDINIZ IÇIN TASARLIYORUZ" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="https://res.cloudinary.com/dnfmvs2ci/image/upload/v1652202397/quinsicon_fuqmoq.png" />
+        <meta name="facebook-domain-verification" content="mc81yaudne9rd4akz4qh4cfk6z9tn2" />
       </Head>
 
       <Header/>
