66cb2c302f4af859c6c13fdab763ed4dd30a08f9
mkngroup <asparpazarlamamehmet@gmail.com>
2022-05-11 16:50:10 +0300

    new gtm new 1

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Header from "../components/header";
 import Main from "../components/main";
 import Footer from "../components/footer";
-import Script from 'next/script'
+import Script from "next/script";
 
 export default function Home() {
   return (
@@ -22,17 +22,15 @@ export default function Home() {
           name="facebook-domain-verification"
           content="mc81yaudne9rd4akz4qh4cfk6z9tn2"
         />
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
+        <Script strategy="afterInteractive">
+          {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-K9SF4XX');
-          `,
-          }}
-        />
+          `}
+        </Script>
       </Head>
 
       <noscript>
