9d9a966857b4a428ee07a2f092959c24f2b1eb09
mkngroup <asparpazarlamamehmet@gmail.com>
2022-05-11 16:35:31 +0300

    second tgm commit

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Header from "../components/header";
 import Main from "../components/main";
 import Footer from "../components/footer";
-import Script from "next/script";
+import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -35,18 +35,18 @@ export default function Home() {
         />
       </Head>
 
-      <Header />
-      <Main />
-      <Footer />
-
       <noscript>
         <iframe
           src="https://www.googletagmanager.com/ns.html?id=GTM-K9SF4XX"
           height="0"
           width="0"
-          style="display:none;visibility:hidden"
+          className="hidden invisible"
         ></iframe>
       </noscript>
+
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
